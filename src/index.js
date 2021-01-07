92b14bb7ba554463d010c75c2b74c5bbba01c77e
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-01-07 21:07:45 +0100

    Add style ti the page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
 import './style.css';
 
 //get a picture
+const resultsShow = document.querySelector("#results");
+const city_name = document.querySelector("#city_name");
 const img = document.querySelector("#weather_img");
 const temperature = document.querySelector("#temperature");
+const weather = document.querySelector("#weather");
 const humidity = document.querySelector("#humidity");
 const windSpeed = document.querySelector("#wind_speed");
 const windDirection = document.querySelector("#wind_direction");
+const err_message = document.querySelector("#error_message");
 const directions = ['North','North-east','East','South-east','South','South-west','West','North-west'];
 
 
 const getPicture = async (theme)=>{
-  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=2EwOKoQXnNF8ez8G3i67obeO2o5TT7EN&s=${theme}`, {mode: 'cors'});
-  const weatherImgData = await response.json();
-  img.src = weatherImgData.data.images.original.url;
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=2EwOKoQXnNF8ez8G3i67obeO2o5TT7EN&s=${theme}`, {mode: 'cors'});
+    const weatherImgData = await response.json();
+    img.src = weatherImgData.data.images.original.url;
+  } catch (error) {
+    img.src = '#';
+    console.error(error);
+  }
 }
 
 const getWeather = async (place)=>{
   // needs pretraitements
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=6e34ebc8632e3e43ecefd3953c9f8c70`, {mode: 'cors'});
-  const weatherData = await response.json();
-  getPicture(weatherData.weather[0].main);
-  temperature.textContent =`${(weatherData.main.temp - 273.15).toFixed(2)}°C`; 
-  humidity.textContent = weatherData.main.humidity;
-  windSpeed.textContent = `${weatherData.wind.speed * 3.6}`;
-  let d = (((weatherData.wind.deg + 360 - 22.5)/45)%8).toFixed(0)
-  windDirection.textContent = directions[d];
-
-  console.log(weatherData);
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=6e34ebc8632e3e43ecefd3953c9f8c70`, {mode: 'cors'});
+    const weatherData = await response.json();
+    const state = weatherData.weather[0].main;
+    getPicture(state);
+    resultsShow.style.display = "block";
+    city_name.textContent = place;
+    weather.textContent = state;
+    temperature.textContent =`${(weatherData.main.temp - 273.15).toFixed(2)}°C`; 
+    humidity.textContent = weatherData.main.humidity;
+    windSpeed.textContent = `${weatherData.wind.speed * 3.6}`;
+    let d = (((weatherData.wind.deg + 360 - 22.5)/45)%8).toFixed(0)
+    windDirection.textContent = directions[d];
+    err_message.style.display = "none"
+    console.log(weatherData);
+  } catch (error) {
+    err_message.style.display = "block";
+    console.error(error);
+  }
 }
 
-getPicture("rain");
-getWeather('Ain Oussera');
+
+// getWeather('Ain Oussera');
 const form = document.querySelector("#town_form");
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
@@ -38,4 +56,3 @@ form.addEventListener('submit',(e)=>{
   getWeather(town);
   console.log(town);
 })
-console.log('Hello youcef');
